053f1e82e7fab9b828085ce4b9b90806c9e80a5c
Chef Kari <oskari.kivekas@student.lut.fi>
2021-04-15 15:49:45 +0300

    workingonit...

--- my-unzip.c
+++ my-unzip.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void unzip(FILE *stream);
+
 int main(int argc, char **argv) {
      FILE *file;
     
@@ -8,16 +10,31 @@ int main(int argc, char **argv) {
         printf("my-unzip: file1 [file2 ...]\n");
         exit(1);
 
-    } else {
+    /*} else {
         for (int i = 1; i < argc; i++) {
             if ((file = fopen(argv[i], "r")) == NULL) {
-      
+                exit(1);
             } 
+            un_zip();
 
       
         } 
-
-
+    */
+    }else{
+        file = fopen("test.txt", "r");
+        unzip(file);
     }
-    return 0;
+return 0;  
+    
+   
+}
+
+void unzip(FILE *stream){
+    char *buffer = NULL;
+    size_t bufsize= 5;
+    int x;
+    
+    while ( x =fread(&buffer, bufsize, 1, stream) > 0){
+        printf("%c\n", buffer);
+    }
 }
