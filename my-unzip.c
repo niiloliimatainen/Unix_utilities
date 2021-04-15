d8d2ffc626c1860e508eeb35c53a8f45c9b588cc
niiloliimatainen <niilo.liimatainen@student.lut.fi>
2021-04-15 13:08:47 +0300

    zip

---
+++ my-unzip.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+     FILE *file;
+    
+    if (argc == 1) {
+        printf("my-unzip: file1 [file2 ...]\n");
+        exit(1);
+
+    } else {
+        for (int i = 1; i < argc; i++) {
+            if ((file = fopen(argv[i], "r")) == NULL) {
+      
+            } 
+
+      
+        } 
+
+
+    }
+    return 0;
+}
